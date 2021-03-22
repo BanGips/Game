7f4dcf82c950efaa45b413e1b7ddea23b2f9b7f4
Sergei Kast <bangips@MacBook-Pro-BanGips.local>
2021-03-22 11:27:28 +0300

    clean project

--- WarFly/WarFly/GameScene.swift
+++ WarFly/WarFly/GameScene.swift
@@ -12,16 +12,14 @@ import CoreMotion
 class GameScene: SKScene {
     
     let screen = UIScreen.main.bounds
-    var player: SKSpriteNode!
-    
-    let motionManager = CMMotionManager()
-    var xAcceleration: CGFloat = 0
+    var player: PlayerPlane!
     
     override func didMove(to view: SKView) {
 
         configStartScene()
         spawnClouds()
         spawnIsland()
+        player.performFly()
     }
     
     private func configStartScene() {
@@ -38,13 +36,6 @@ class GameScene: SKScene {
         player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
         self.addChild(player)
         
-        motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-            if let data = data {
-                let acceleration = data.acceleration
-                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
-            }
-        }
     }
     
     private func spawnClouds() {
@@ -76,13 +67,7 @@ class GameScene: SKScene {
     override func didSimulatePhysics() {
         super.didSimulatePhysics()
         
-        player.position.x += xAcceleration * 50
-        
-        if player.position.x < -70 {
-            player.position.x = self.size.width + 70
-        } else if player.position.x > self.size.width + 70 {
-            player.position.x = -70
-        }
+        player.checkPosition()
         
         enumerateChildNodes(withName: "backgroundSprite") { (node, _) in
             if node.position.y < -150 {
