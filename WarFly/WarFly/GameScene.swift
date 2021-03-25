2f408ab63f9763d3ec7781a78ecfed2497df3eb2
Sergei Kast <bangips@MacBook-Pro-BanGips.local>
2021-03-25 11:30:03 +0300

    user interface

--- WarFly/WarFly/GameScene.swift
+++ WarFly/WarFly/GameScene.swift
@@ -13,6 +13,12 @@ class GameScene: SKScene {
     
     let screen = UIScreen.main.bounds
     var player: PlayerPlane!
+    let scoreBaclground = SKSpriteNode(imageNamed: "scores")
+    let scoreLabel = SKLabelNode(text: "1000")
+    let menuButton = SKSpriteNode(imageNamed: "menu")
+    let life1 = SKSpriteNode(imageNamed: "life")
+    let life2 = SKSpriteNode(imageNamed: "life")
+    let life3 = SKSpriteNode(imageNamed: "life")
     
     override func didMove(to view: SKView) {
 
@@ -24,7 +30,37 @@ class GameScene: SKScene {
         player.performFly()
         spawnPowerUp()
         spanwEnemies()
+        setupUI()
         
+        
+    }
+    
+    private func setupUI() {
+        scoreBaclground.position = CGPoint(x: scoreBaclground.size.width + 10, y: self.size.height - scoreBaclground.size.height / 2 - 10)
+        scoreBaclground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+        scoreBaclground.zPosition = 99
+        self.addChild(scoreBaclground)
+        
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.verticalAlignmentMode = .center
+        scoreLabel.position = CGPoint(x: -10, y: 3)
+        scoreLabel.zPosition = 100
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 30
+        scoreBaclground.addChild(scoreLabel)
+        
+        menuButton.position = CGPoint(x: 20, y: 20)
+        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        menuButton.zPosition = 100
+        self.addChild(menuButton)
+        
+        let lifes = [life1, life2, life3]
+        for (index, life) in lifes.enumerated() {
+            life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 20)
+            life.zPosition = 100
+            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+            self.addChild(life)
+        }
     }
     
     private func spawnSpiralOfEnemies() {
